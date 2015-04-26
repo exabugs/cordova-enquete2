081051475616fb637c1d810c28a123261cb90eda
exabugs <exabugs@gmail.com>
2015-04-27 00:13:49 +0900

    first commit.

--- src/js/index.js
+++ src/js/index.js
@@ -17,10 +17,14 @@
  * under the License.
  */
 
-var chiwawa = "https://demo.chiwawa.jp";
-//var chiwawa = "http://192.168.11.4:8004";
+//var chiwawa = "https://demo.chiwawa.jp";
+var chiwawa = "http://192.168.11.4:8004";
+
+
+var TOKEN_KEY = "access_token";
 
 var app = {
+
   // Application Constructor
   initialize: function () {
     this.bindEvents();
@@ -30,26 +34,34 @@ var app = {
   // Bind any events that are required on startup. Common events are:
   // 'load', 'deviceready', 'offline', and 'online'.
   bindEvents: function () {
-    document.addEventListener("deviceready", this.onDeviceReady, false);
+    $(document).on("deviceready", this.onDeviceReady);
   },
   // deviceready Event Handler
   //
   // The scope of 'this' is the event. In order to call the 'receivedEvent'
   // function, we must explicitly call 'app.receivedEvent(...);'
   onDeviceReady: function () {
-    app.receivedEvent("deviceready");
+    console.log("onDeviceReady");
 
+    app.receivedEvent("deviceready");
+  },
+
+  logout: function () {
+    if (localStorage.getItem(TOKEN_KEY)) {
+      app.ajax({url: '/logout'});
+      localStorage.removeItem(TOKEN_KEY);
+    }
+    $("#logs").append("<p class='success'><b>logout: </b>" + "</p>");
+  },
+
+  login: function () {
+    // todo: アプリ内で設定変更できるように
     var params = {
       url: chiwawa + "/oauth2/",
       client_id: "cordovaHello",
       client_secret: "chiwawaapp..secret",
       redirect_uri: "http://chiwawa.jp/auth/callback/"
     };
-
-    var accessToken = window.localStorage.getItem("access_token");
-    $("#logs").append("<p class='success'><b>access_token: </b>" + accessToken + "</p>");
-
-
     var oauthParams = {
       response_type: "code",               // required - "code"/"token"
       auth_url: params.url + "authorize",  // required
@@ -60,35 +72,67 @@ var app = {
       // other_params: {}        // optional params object for scope, state, display...
       // logout_url: "",         // recommended if available
     };
-
     $.oauth2(oauthParams,
       function (token, response) {
         // do something with token or response
         $("#logs").append("<p class='success'><b>access_token: </b>" + token + "</p>");
-        $("#logs").append("<p class='success'><b>response: </b>" + JSON.stringify(response) + "</p>");
-
-        window.localStorage.setItem("access_token", token);
+        localStorage.setItem(TOKEN_KEY, token);
       },
-      function (error, response) {
-        // do something with error object
-        $("#logs").append("<p class='error'><b>error: </b>" + JSON.stringify(error) + "</p>");
-        $("#logs").append("<p class='error'><b>response: </b>" + JSON.stringify(response) + "</p>");
+      function (err, response) {
+        // do something with err object
+        $("#logs").append("<p class='error'><b>error: </b>" + JSON.stringify(err) + "</p>");
+        localStorage.removeItem(TOKEN_KEY);
       }
     );
-
   },
+
+  getUser: function () {
+    app.ajax({
+      url: '/oauth2/token',
+      success: function (data) {
+        $("#logs").append("<p class='success'><b>response: </b>" + JSON.stringify(data) + "</p>");
+      },
+      error: function (error) {
+        $("#logs").append("<p class='error'><b>response: </b>" + JSON.stringify(error) + "</p>");
+      }
+    });
+  },
+
+  ajax: function (params) {
+    var token = localStorage.getItem(TOKEN_KEY);
+    if (!token) {
+      app.login();
+    } else {
+      $.ajax({
+        headers: {
+          'Authorization': "Bearer " + token
+        },
+        url: chiwawa + params.url,
+        data: params.data,
+        type: params.type || 'GET',
+        success: function (data) {
+          params.success(data);
+        },
+        error: function (err) {
+          params.error(err);
+        }
+      });
+    }
+  },
+
   // Update DOM on a Received Event
   receivedEvent: function (id) {
-    var parentElement = document.getElementById(id);
-    var listeningElement = parentElement.querySelector(".listening");
-    var receivedElement = parentElement.querySelector(".received");
+    //var parentElement = document.getElementById(id);
+    //var listeningElement = parentElement.querySelector(".listening");
+    //var receivedElement = parentElement.querySelector(".received");
+    //listeningElement.setAttribute("style", "display:none;");
+    //receivedElement.setAttribute("style", "display:block;");
 
-    listeningElement.setAttribute("style", "display:none;");
-    receivedElement.setAttribute("style", "display:block;");
-
-    $.each(document.querySelectorAll("td.device"), function () {
-      $(this).text(device[this.id]);
-    });
+    if (device) {
+      $.each(document.querySelectorAll("td.device"), function () {
+        $(this).text(device[this.id]);
+      });
+    }
 
     console.log("Received Event: " + id);
   }
